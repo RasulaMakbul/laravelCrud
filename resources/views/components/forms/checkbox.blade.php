6a4d03736f1d1a7ab8930029ff12b6ed8455c3ca
RasulaMakbul <emu.cfs@gmail.com>
2022-10-04 10:37:41 +0600

    radio

--- resources/views/components/forms/checkbox.blade.php
+++ resources/views/components/forms/checkbox.blade.php
@@ -4,7 +4,7 @@
 @foreach($radioUnit as $key=>$item)
 
 <div class="form-group m-3">
-    <input type="checkbox" name="{{ $name }}" {{ $attributes->merge(['class'=>'form-checkbox'])}} id="{{ $key }}Input" value="{{$item}}" @if (in_array($item, $setItemr)) checked @endif>
+    <input type="radio" name="{{ $name }}" {{ $attributes->merge(['class'=>'form-checkbox'])}} id="{{ $key }}Input" value="{{$item}}" @if (in_array($item, $setItemr)) checked @endif>
 
     <label class="form-check-label" for="{{ $key }}Input">{{ $item }}</label>
 
